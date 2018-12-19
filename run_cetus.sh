545332221229ea0377f0859fcd230423811adef5
abwolf <abwolf@uw.edu>
2018-12-19 14:23:56 -0500

    update

--- run_cetus.sh
+++ run_cetus.sh
@@ -13,7 +13,7 @@ date
 dir=/Genomics/grid/users/abwolf/SimulatedDemographic/MatchPvalue/bin
 snakefile=$( echo $dir/Snakefile)
 clusterconfigfile=$( echo $dir/cetus_cluster.yaml )
-configfile=$( echo $dir/config_n1_0.1_mAfB_0.0_mBAf_0.0_mAfEu_0.0_mEuAf_0.000025.yaml )
+configfile=$( echo $dir/config_n1_0.1_mAfB_0.0_mBAf_0.0_mAfEu_0.0_mEuAf_0.0005.yaml )
 
 ############
 echo $dir
